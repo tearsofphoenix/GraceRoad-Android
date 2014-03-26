0705b687d3839820dc88baee791d5f95701adfb1
tearsofphoenix <tearsofphoenix@icloud.com>
2014-03-26 15:48:50 +0800

    working on.

--- src/com/church/GraceRoad/IntroduceView.java
+++ src/com/church/GraceRoad/IntroduceView.java
@@ -2,6 +2,10 @@ package com.church.GraceRoad;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,5 +21,17 @@ public class IntroduceView extends ContentView
         super();
         setTitle("新松江恩典教会");
         setBackgroundColor(Color.RED);
+        //LayoutInflater inflater = (LayoutInflater)this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        //View view = inflater.inflate(R.layout.introduce, null);
+//        View view  = LayoutInflater.from(this.getContext()).inflate(R.layout.introduce, null);
+//        Log.e(GR.LogTag, view.toString());
+//
+//        this.addView(view);
+
+        UILabel titleLabel = new UILabel();
+        titleLabel.setFrame(new Rect(0, 0, 320, 40));
+        titleLabel.setText(R.string.introduce_title0);
+        addView(titleLabel);
+
     }
 }
