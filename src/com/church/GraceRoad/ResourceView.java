d02489cfafcb8ae4e55382169bb857bf0d652d96
tearsofphoenix <tearsofphoenix@icloud.com>
2014-04-09 16:46:15 +0800

    will sync.

---
+++ src/com/church/GraceRoad/ResourceView.java
@@ -0,0 +1,8 @@
+package com.church.GraceRoad;
+
+/**
+ * Created by Mac003 on 14-4-9.
+ */
+public class ResourceView
+{
+}
---
+++ src/pl/polidea/sectionedlist/StandardArrayAdapter.java
@@ -0,0 +1,8 @@
+package pl.polidea.sectionedlist;
+
+/**
+ * Created by Mac003 on 14-4-9.
+ */
+public class StandardArrayAdapter
+{
+}
